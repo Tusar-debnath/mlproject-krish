6b0a6b42d68abac4c391dd7254b6f2794ff1e223
Tusar Debnath <tushardebnath2000@gmail.com>
2025-08-07 21:23:40 +0530

    Data Ingestion

--- source/exception.py
+++ source/exception.py
@@ -7,7 +7,7 @@ def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename   #we can find the documnetation online by searching python exception handling
     error_message = "Error occured in python script name[{0}] line number [{1}] error massage[{2}]".format(
-        file_name, exc_tb, str(error)
+        file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
 
